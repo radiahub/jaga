e7179808f09f1d1055093680ac179e4dc0ce161e
radiaHUB <radiahub@gmail.com>
2026-08-17 19:07:29 +0700

    Change security policy to accomodate mapbox

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,35 @@
+// Declare notification click handler
+//
+self.addEventListener('notificationclick', (event)=>{
+
+    event.notification.close();
+    const msgId = event.notification.data.msg;
+    const url = (msgId.length > 0) ? `/jaga/index.html?msg=${encodeURIComponent(msgId)}` : `/jaga/index.html`;
+    
+    event.waitUntil(
+        clients
+        .matchAll({
+            type: 'window',
+            includeUncontrolled: true
+        })
+        .then((clientList) => {
+            for (const client of clientList) {
+                if ((client.url.indexOf('/jaga') >= 0) && ('focus' in client)) {
+                    return client.focus();
+                }
+            }
+    
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
+    
+});
+
+
 // Import Firebase scripts
+//
 self.importScripts('https://www.gstatic.com/firebasejs/11.3.1/firebase-app-compat.js');
 self.importScripts('https://www.gstatic.com/firebasejs/11.3.1/firebase-messaging-compat.js');
 
@@ -25,11 +56,18 @@ const messaging = firebase.messaging();
 
 // Handle background push notifications
 messaging.onBackgroundMessage((payload)=>{
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/jaga/glint-transparent.png'
+    
+  const title = 'Jaga';
+  const body = payload.data.text;
+  const options = {
+    body : body,
+    icon : '/jaga/jaga_256x256.png',
+    tag  : 'unique-message-tag',
+    data : {
+        msg : payload.data.msg || ''
+    }
   };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    
+  return self.registration.showNotification(title, options);
 });
 
